eedc27742a56d99d573065742f6432ef71993a62
Casey Foster <c@sey.me>
2026-05-28 21:27:39 -0500

    Clean up

--- src/cli.js
+++ src/cli.js
@@ -100,161 +100,156 @@ ${Object.entries(options)
   process.exit(errorMessage ? 1 : 0);
 };
 
-(() => {
-  let positionals;
-  let values;
+let positionals;
+let values;
 
+try {
+  ({ positionals, values } = parseArgs({
+    allowNegative: true,
+    allowPositionals: true,
+    options,
+    strict: true
+  }));
+} catch (er) {
+  throw showHelp(er instanceof Error ? er.message : 'Unknown Error');
+}
+
+const [command, ...args] = positionals;
+
+if (values.help) throw showHelp();
+
+if (values.version) {
+  console.log(version);
+  process.exit();
+}
+
+let {
+  'init-spawn': initSpawn,
+  'keep-alive': keepAlive,
+  'shutdown-signal': shutdownSignal,
+  'reload-signal': reloadSignal = shutdownSignal,
+  'restart-after-signal': restartAfterSignal,
+  debounce: _debounce,
+  restart,
+  silent: onlyErrors,
+  wait: _wait,
+  watch: patterns
+} = values;
+
+const debounce = _debounce ? parseFloat(_debounce) : undefined;
+const wait = _wait ? parseFloat(_wait) : undefined;
+
+/**
+ * @param {'error' | 'info' | 'success'} type
+ * @param {string} message
+ */
+const log = (type, message) => {
+  if (onlyErrors && type !== 'error') return;
+
+  message = styleText(
+    type === 'error' ? 'red' : type === 'success' ? 'green' : [],
+    `[${type}] ${message}`
+  );
+  console[type === 'error' ? 'error' : 'log'](message);
+};
+
+if (!command) throw showHelp('Please specify a command.');
+
+const runLabel = getRunLabel([command, ...args]);
+/** @type {ChildProcess | undefined} */
+let child;
+/** @type {NodeJS.Timeout | undefined} */
+let sigkillTimeoutId;
+let state = 'dead';
+/** @type {ReturnType<typeof watch>} */
+let closeWatcher;
+
+/** @param {Error} er */
+const handleError = er => log('error', er.message);
+
+/**
+ * @param {number} code
+ * @param {NodeJS.Signals | undefined} signal
+ */
+const handleClose = (code, signal) => {
+  child = undefined;
+  if (signal) {
+    log(signal === shutdownSignal ? 'info' : 'error', `Killed with ${signal}`);
+  }
+  if (code === 0) log('success', 'Exited cleanly');
+  if (code > 0) log('error', `Exited with code ${code}`);
+  const rerun = (state !== 'unsignaled' && restartAfterSignal) || keepAlive;
+  state = 'dead';
+  if (rerun) run();
+};
+
+/** @param {string[] | undefined} [paths] */
+const run = (paths = []) => {
+  if (state !== 'dead') {
+    if (state === 'unsignaled') kill();
+    return;
+  }
+
+  log('info', runLabel);
+  state = 'unsignaled';
+  child = spawn(command, args, {
+    env: { WATCHY_PATHS: paths.join(','), ...env },
+    stdio: ['ignore', 1, 2]
+  })
+    .on('error', handleError)
+    .on('close', handleClose);
+};
+
+/** @param {boolean} [terminate] */
+const kill = (terminate = false) => {
+  if (state === 'dead' || state === 'terminating') return;
+
+  if (!terminate) terminate = !reloadSignal;
+
+  if (terminate && state === 'unsignaled' && wait) {
+    sigkillTimeoutId = setTimeout(sigkill, wait * 1000);
+    child?.once('close', () => clearTimeout(sigkillTimeoutId));
+  }
+
+  const signal = terminate ? shutdownSignal : reloadSignal;
+  log('info', `Sending ${signal}...`);
+  state = terminate ? 'terminating' : 'reloading';
+  child?.kill(/** @type {NodeJS.Signals} */ (signal));
+};
+
+const sigkill = () => {
+  log('error', `Failed to kill with ${shutdownSignal} after ${wait} seconds`);
+  child?.kill('SIGKILL');
+};
+
+const shutdown = () => {
+  process.stdin.pause();
+  if (closeWatcher) closeWatcher();
+  keepAlive = false;
+  restartAfterSignal = false;
+  kill(true);
+  if (child) child.once('close', () => process.exit());
+  else process.exit();
+};
+
+if (patterns) {
   try {
-    ({ positionals, values } = parseArgs({
-      allowNegative: true,
-      allowPositionals: true,
-      options,
-      strict: true
-    }));
+    closeWatcher = watch({ debounce, onChange: run, patterns });
   } catch (er) {
-    return showHelp(er instanceof Error ? er.message : 'Unknown Error');
+    handleError(/** @type {Error} */ (er));
+    process.exit(1);
   }
+}
 
-  const [command, ...args] = positionals;
+if (restart) {
+  process.stdin.setEncoding('utf8');
+  process.stdin.resume();
+  process.stdin.on('data', data => {
+    if (data.toString().trim() === restart) run();
+  });
+}
 
-  if (values.help) return showHelp();
+if (initSpawn) run();
 
-  if (values.version) {
-    console.log(version);
-    process.exit();
-  }
-
-  let {
-    'init-spawn': initSpawn,
-    'keep-alive': keepAlive,
-    'shutdown-signal': shutdownSignal,
-    'reload-signal': reloadSignal = shutdownSignal,
-    'restart-after-signal': restartAfterSignal,
-    debounce: _debounce,
-    restart,
-    silent: onlyErrors,
-    wait: _wait,
-    watch: patterns
-  } = values;
-
-  const debounce = _debounce ? parseFloat(_debounce) : undefined;
-  const wait = _wait ? parseFloat(_wait) : undefined;
-
-  /**
-   * @param {'error' | 'info' | 'success'} type
-   * @param {string} message
-   */
-  const log = (type, message) => {
-    if (onlyErrors && type !== 'error') return;
-
-    message = styleText(
-      type === 'error' ? 'red' : type === 'success' ? 'green' : [],
-      `[${type}] ${message}`
-    );
-    console[type === 'error' ? 'error' : 'log'](message);
-  };
-
-  if (!command) return showHelp('Please specify a command.');
-
-  const runLabel = getRunLabel([command, ...args]);
-  /** @type {ChildProcess | undefined} */
-  let child;
-  /** @type {NodeJS.Timeout | undefined} */
-  let sigkillTimeoutId;
-  let state = 'dead';
-  /** @type {ReturnType<typeof watch>} */
-  let closeWatcher;
-
-  /** @param {Error} er */
-  const handleError = er => log('error', er.message);
-
-  /**
-   * @param {number} code
-   * @param {NodeJS.Signals | undefined} signal
-   */
-  const handleClose = (code, signal) => {
-    child = undefined;
-    if (signal) {
-      log(
-        signal === shutdownSignal ? 'info' : 'error',
-        `Killed with ${signal}`
-      );
-    }
-    if (code === 0) log('success', 'Exited cleanly');
-    if (code > 0) log('error', `Exited with code ${code}`);
-    const rerun = (state !== 'unsignaled' && restartAfterSignal) || keepAlive;
-    state = 'dead';
-    if (rerun) run();
-  };
-
-  /** @param {string[] | undefined} [paths] */
-  const run = (paths = []) => {
-    if (state !== 'dead') {
-      if (state === 'unsignaled') kill();
-      return;
-    }
-
-    log('info', runLabel);
-    state = 'unsignaled';
-    child = spawn(command, args, {
-      env: { WATCHY_PATHS: paths.join(','), ...env },
-      stdio: ['ignore', 1, 2]
-    })
-      .on('error', handleError)
-      .on('close', handleClose);
-  };
-
-  /** @param {boolean} [terminate] */
-  const kill = (terminate = false) => {
-    if (state === 'dead' || state === 'terminating') return;
-
-    if (!terminate) terminate = !reloadSignal;
-
-    if (terminate && state === 'unsignaled' && wait) {
-      sigkillTimeoutId = setTimeout(sigkill, wait * 1000);
-      child?.once('close', () => clearTimeout(sigkillTimeoutId));
-    }
-
-    const signal = terminate ? shutdownSignal : reloadSignal;
-    log('info', `Sending ${signal}...`);
-    state = terminate ? 'terminating' : 'reloading';
-    child?.kill(/** @type {NodeJS.Signals} */ (signal));
-  };
-
-  const sigkill = () => {
-    log('error', `Failed to kill with ${shutdownSignal} after ${wait} seconds`);
-    child?.kill('SIGKILL');
-  };
-
-  const shutdown = () => {
-    process.stdin.pause();
-    if (closeWatcher) closeWatcher();
-    keepAlive = false;
-    restartAfterSignal = false;
-    kill(true);
-    if (child) child.once('close', () => process.exit());
-    else process.exit();
-  };
-
-  if (patterns) {
-    try {
-      closeWatcher = watch({ debounce, onChange: run, patterns });
-    } catch (er) {
-      handleError(/** @type {Error} */ (er));
-      process.exit(1);
-    }
-  }
-
-  if (restart) {
-    process.stdin.setEncoding('utf8');
-    process.stdin.resume();
-    process.stdin.on('data', data => {
-      if (data.toString().trim() === restart) run();
-    });
-  }
-
-  if (initSpawn) run();
-
-  process.once('SIGTERM', shutdown);
-  process.once('SIGINT', shutdown);
-})();
+process.once('SIGTERM', shutdown);
+process.once('SIGINT', shutdown);
